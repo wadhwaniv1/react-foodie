1b9af59a446c7350dce9e69b1cc307459b59ec59
Vishal <wadhwaniv1@gmail.com>
2021-11-17 00:14:14 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css'
+import HomePage from './HomePage';
+import DetailPage from './DetailPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/FoodCard.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function FoodCard(props) {
+    return (
+        <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 foodlist">
+            <a href="./home.html" class="card-link">
+                <div class="card">
+                    <img class="card-img-top"
+                        src={props.imageUrl}
+                        alt="Card image cap" />
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <h4 class="dish-name">{props.foodName}</h4>
+                            </div>
+                            <div class="col">
+                                <p class="rating">{props.ratting} <i class="fa fa-star" ></i></p>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="dish-details">{props.resName}</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    )
+}
+
+export default FoodCard
